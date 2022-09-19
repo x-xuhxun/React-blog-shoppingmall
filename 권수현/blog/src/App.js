85efc05f4e99867559e65176510ebf7e910f4a4b
x-xuhxun <hiihii0402@gmail.com>
2022-09-20 00:00:04 +0900

    9/19

--- 권수현/blog/src/App.js
+++ 권수현/blog/src/App.js
@@ -1,17 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  let post = '강남 우동 맛집'; 
+  let post = '강남우동맛집';
+  let [글제목, b] = useState(['남자코트 추천', '강남우동맛집', '파이썬독학']);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{color : 'red', fontSize: '32px'}}>블로그임</h4>
+        <h4>ReactBlog</h4>
+      </div>  
+      <div className="list">
+        <h4>{글제목[0]}</h4>
+        <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>글제목</h4>
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
